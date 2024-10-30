6a42de50bfb4c6d8270c2c8d55af5c2ccdc56b71
Ritu <singhritu241101@gmail.com>
2024-10-30 21:29:21 +0530

    fix: removed bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,13 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
 const { setUser, getUser } = require("../service/auth");
 
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
   });
   const token = setUser(user);
 
